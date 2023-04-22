207e1c7fa49d6e33aa70466ca8aefdb562a05072
Kermalis <29823718+Kermalis@users.noreply.github.com>
2023-04-22 19:19:32 -0400

    Initial commit

---
+++ MIDIProgramSplitter/OutMIDI.cs
@@ -0,0 +1,78 @@
+﻿using Kermalis.MIDI;
+using System;
+using System.IO;
+
+namespace MIDIProgramSplitter;
+
+internal sealed class OutMIDI
+{
+	private readonly MIDIFile _midi;
+
+	public OutMIDI(MIDIFile inMIDI)
+	{
+		Console.WriteLine();
+
+		MIDIFormat format = inMIDI.HeaderChunk.Format;
+		Console.WriteLine("MIDI Format: " + format);
+		if (format != MIDIFormat.Format1)
+		{
+			throw new Exception("MIDI is not Format1. Only Format1 is supported for now.");
+		}
+
+		TimeDivisionValue timeDiv = inMIDI.HeaderChunk.TimeDivision;
+		Console.WriteLine("MIDI TimeDiv: " + timeDiv);
+
+		_midi = new MIDIFile(MIDIFormat.Format1, timeDiv, 1);
+		// TODO: Only Format1 is supported right now, but the program is mostly built to support all. Just some weird tempo/timesig duplication needs to be fixed
+
+		byte trackIndex = 0;
+		foreach (MIDITrackChunk track in inMIDI.EnumerateTrackChunks())
+		{
+			// Meta track special case
+			if (trackIndex == 0 && format == MIDIFormat.Format1)
+			{
+				// Check first. The Colress MIDI I have has the first track with notes and tempo/timesig info, which is wrong.
+				if (TrackHasNonMetaMessages(track))
+				{
+					throw new Exception($"Format1 MIDI must have a {nameof(MetaMessage)} track as the first track. This MIDI included other messages in the first track, which is invalid");
+				}
+
+				Console.WriteLine("Copying Format1 meta track");
+				_midi.AddChunk(track);
+			}
+			else
+			{
+				var td = new TrackData(trackIndex, track, this);
+				td.SplitTrack();
+			}
+			trackIndex++;
+		}
+	}
+
+	private static bool TrackHasNonMetaMessages(MIDITrackChunk t)
+	{
+		for (MIDIEvent? e = t.First; e is not null; e = e.Next)
+		{
+			if (e.Message is not MetaMessage)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public MIDITrackChunk Create()
+	{
+		var t = new MIDITrackChunk();
+		_midi.AddChunk(t);
+		return t;
+	}
+
+	public void Save(string outFile)
+	{
+		using (FileStream fs = File.Create(outFile))
+		{
+			_midi.Save(fs);
+		}
+	}
+}
---
+++ MIDIProgramSplitter/TrackData.cs
@@ -0,0 +1,136 @@
+﻿using Kermalis.MIDI;
+using System;
+using System.Collections.Generic;
+
+namespace MIDIProgramSplitter;
+
+// MIDI Format 0 would have multiple channels per track. 1 and 2 do not
+internal sealed class TrackData
+{
+	private readonly byte _trackIndex;
+	private readonly MIDITrackChunk _inTrack;
+
+	/// <summary>Per channel. Each HashSet keeps track of the programs that have NoteOn</summary>
+	private readonly HashSet<MIDIProgram>[] _usedPrograms;
+	/// <summary>Used while iterating the events</summary>
+	private readonly MIDIProgram[] _curPrograms;
+	private readonly TrackSplit?[] _newTracks;
+
+	/// <summary>Scavenges <paramref name="inTrack"/> for events and splits them into new tracks for <paramref name="outMIDI"/></summary>
+	public TrackData(byte trackIndex, MIDITrackChunk inTrack, OutMIDI outMIDI)
+	{
+		_trackIndex = trackIndex;
+		_inTrack = inTrack;
+
+		_usedPrograms = new HashSet<MIDIProgram>[16];
+		_curPrograms = new MIDIProgram[16];
+
+		for (byte c = 0; c < 16; c++)
+		{
+			_usedPrograms[c] = new HashSet<MIDIProgram>();
+		}
+
+		RecordAllUsedProgramsInTrack();
+
+		_newTracks = new TrackSplit?[16];
+		for (byte c = 0; c < 16; c++)
+		{
+			HashSet<MIDIProgram> used = _usedPrograms[c];
+			_newTracks[c] = used.Count == 0 ? null : new TrackSplit(trackIndex, c, used, outMIDI);
+		}
+	}
+
+	/// <summary>Searches for all program changes in <see cref="_inTrack"/>. <see cref="_usedPrograms"/>'s elements correspond to each MIDI channel. Program changes with no NoteOn are not recorded.</summary>
+	private void RecordAllUsedProgramsInTrack()
+	{
+		for (MIDIEvent? e = _inTrack.First; e is not null; e = e.Next)
+		{
+			switch (e.Message)
+			{
+				case ProgramChangeMessage m:
+				{
+					byte chan = m.Channel;
+					_curPrograms[chan] = m.Program;
+					break;
+				}
+				case NoteOnMessage m:
+				{
+					byte chan = m.Channel;
+					_usedPrograms[chan].Add(_curPrograms[chan]);
+					break;
+				}
+			}
+		}
+	}
+
+	/// <summary>Iterates all messages in <see cref="_inTrack"/> and copies/splits them to the new tracks</summary>
+	public void SplitTrack()
+	{
+		Array.Clear(_curPrograms);
+
+		for (MIDIEvent? e = _inTrack.First; e is not null; e = e.Next)
+		{
+			if (SplitTrack_SpecialMessage(e))
+			{
+				continue;
+			}
+
+			if (e.Message is IMIDIChannelMessage cm)
+			{
+				// Add to all new tracks on this channel
+				_newTracks[cm.Channel]?.InsertEventIntoAllNewTracks(e);
+			}
+			else // Probably a global message like EndOfTrack
+			{
+				// Add to all new tracks
+				foreach (TrackSplit? ts in _newTracks)
+				{
+					ts?.InsertEventIntoAllNewTracks(e);
+				}
+			}
+		}
+	}
+	/// <summary>Returns true if this event should not be added to the new tracks</summary>
+	private bool SplitTrack_SpecialMessage(MIDIEvent e)
+	{
+		MIDIMessage msg = e.Message;
+		switch (msg)
+		{
+			case ProgramChangeMessage m:
+			{
+				byte chan = m.Channel;
+				_curPrograms[chan] = m.Program;
+				TrackSplit? ts = _newTracks[chan]; // May be null if there are no notes and no other program changes
+
+				if (ts is null || !ts.VoiceIsUsed(m.Program))
+				{
+					return true; // Ignore this program change in that case
+				}
+				break; // Add to all new tracks on this channel
+			}
+			case NoteOnMessage:
+			case NoteOffMessage:
+			{
+				var m = (IMIDIChannelMessage)msg;
+				byte chan = m.Channel;
+				MIDIProgram curVoice = _curPrograms[chan];
+				TrackSplit ts = _newTracks[chan]!; // Not null if there are notes
+
+				ts.InsertEventIntoNewTrack(e, curVoice);
+				return true; // Only add to correct new track
+			}
+			case MetaMessage m:
+			{
+				Console.WriteLine("Track {0}: {1}", _trackIndex, m);
+
+				if (m.Type == MetaMessageType.TrackName)
+				{
+					return true; // Ignore these
+				}
+				break; // Add to all new tracks
+			}
+		}
+
+		return false;
+	}
+}
