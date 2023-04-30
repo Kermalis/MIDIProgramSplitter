430d5ece2610e0790b03597cef9e179c6365737f
Kermalis <29823718+Kermalis@users.noreply.github.com>
2023-04-30 17:37:40 -0400

    Sort notes by AbsoluteTick as a failsafe

--- MIDIProgramSplitter/FLP/FLPattern.cs
+++ MIDIProgramSplitter/FLP/FLPattern.cs
@@ -14,6 +14,9 @@ internal sealed class FLPattern
 
 	public void WritePatternNotes(EndianBinaryWriter w)
 	{
+		// Must be in order of AbsoluteTick
+		Notes.Sort((n1, n2) => n1.AbsoluteTick.CompareTo(n2.AbsoluteTick));
+
 		w.WriteEnum(FLEvent.PatternNotes);
 		FLProject.WriteTextEventLength(w, (uint)Notes.Count * FLPatternNote.LEN);
 		foreach (FLPatternNote note in Notes)
